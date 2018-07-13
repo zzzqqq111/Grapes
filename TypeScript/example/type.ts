be9c014cfd83a362f4b8e4cbd36c691e9e0267f1
Buggy <417274186@qq.com>
2018-07-13 10:20:21 +0800

    update: 错误

--- TypeScript/example/type.ts
+++ TypeScript/example/type.ts
@@ -6,13 +6,11 @@
 // }
 
 /* ========================= */
-let a: number 
-let add = ( a : number, b: number ): number =>{
-    return a + b
-}
-a = add(1, 2)
-console.log(a)
-a = add('1', 2)
-console.log(a)
-✘ 😏  ~/Grapes   master ●  tsc ./TypeScript/example/type.ts
-TypeScript/example/type.ts:15:9 - error TS2345: Argument of type '"1"' is not assignable to parameter of type 'number'.
+// let a: number 
+// let add = ( a : number, b: number ): number =>{
+//     return a + b
+// }
+// a = add(1, 2)
+// console.log(a)
+// a = add('1', 2)
+// console.log(a)
